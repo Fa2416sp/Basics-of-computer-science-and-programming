39d55ace3437a62bbea6aee25049ac3d5709695d
Alex <56237625+Fa2416sp@users.noreply.github.com>
2024-09-10 22:15:26 +0300

    Add files via upload

---
+++ inf/task1/main.c
@@ -0,0 +1,17 @@
+/**
+ * main.c -- программа "Hello, students!"
+ *
+ * Copyright (c) 2022, alex <alex-tabota@yandex.ru>
+ *
+ * This code is licensed under MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    /* Выводим приветствие */
+    fprintf(stdout, "Hello, students!\n");
+
+    return 0;
+}
